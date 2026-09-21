2bd7c8ea65f15d4cf43f5c129c1bc70e1d853bfa
Your Name <you@example.com>
2026-09-21 21:36:25 +0800

    part6 exercise 19-23

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,18 +1,21 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useAnecdotes } from './hooks/useAnecdotes'
 
 const App = () => {
+  const { anecdotes, isPending, isError, vote } = useAnecdotes()
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    vote(anecdote)
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  if (isPending) {
+    return <div>loading data...</div>
+  }
+
+  if (isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
 
   return (
     <div>
--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,12 @@
+import { useAnecdotes } from '../hooks/useAnecdotes'
 const AnecdoteForm = () => {
+  const { addAnecdote: addAnecdoteToServer } = useAnecdotes()
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.reset()
-    console.log('new anecdote')
+    addAnecdoteToServer(content)
   }
 
   return (
